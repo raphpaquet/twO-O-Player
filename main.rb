c6595f20dda0362dd409445e8fa456760bc72878
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-04 18:54:00 +0000

    final code

--- main.rb
+++ main.rb
@@ -7,55 +7,47 @@ puts "* * * * ! WELCOME TO THE MATH GAME ! * * * * *"
 puts "*                                            *"
 puts "**********************************************"
 puts
-puts "Please enter Player 1's name: "
-player1_name = gets.chomp
-puts "Please enter Player 2's name: "
-player2_name = gets.chomp
 
-player1 = Players.new(player1_name, 3)
-player2 = Players.new(player2_name, 3)
+puts "Please enter Player 1's name: "
+@player1_name = gets.chomp
+puts "Please enter Player 2's name: "
+@player2_name = gets.chomp
+
+@player1 = Players.new(@player1_name, 3)
+@player2 = Players.new(@player2_name, 3)
 
 puts "* * * * * ! LET'S START THE GAME ! * * * * * *"
 
-
-
-
-until player1.gameover || player2.gameover
+def turns(current_player)
   questions = Questions.new
-  puts "#{player1_name}: #{questions.show_question}"
+  puts "#{current_player.name}: #{questions.show_question}"
+
   player_answer = gets.chomp
+
     if player_answer == "#{questions.answer}"
-      puts "#{player1_name}: YES! You are correct."
+      puts "#{current_player.name}: YES! You are correct."
     else 
-      puts "#{player1_name}: SERIOUSLY? No !"
-      player1.incorrect
-    end
-    if player1.gameover
-      puts "#{player2_name} WINS with a score of #{player2.lives}/3"
-      puts "- - - - - - - - !  GAMEOVER  ! - - - - - - - -"
-      puts "GoodBye ! "
-    else 
-      puts "#{player1_name}: #{player1.lives}/3 vs #{player2_name}: #{player2.lives}/3"
-      puts "* * * * * * * !  NEW TURN   ! * *  * * * * * *"
-      puts 
+      puts "#{current_player.name}: SERIOUSLY? No !"
+    current_player.incorrect
     end
 
-  questions2 = Questions.new
-  puts "#{player2_name}: #{questions2.show_question}"
-  player_answer = gets.chomp
-    if player_answer == "#{questions2.answer}"
-      puts "#{player2_name}: YES! You are correct."
-    else 
-      puts "#{player2_name}: SERIOUSLY? No !"
-      player2.incorrect
-      if player2.gameover
-        puts "#{player1_name} WINS with a score of #{player1.lives}/3"
-        puts "- - - - - - - - !  GAMEOVER  ! - - - - - - - -"
-        puts "GoodBye ! "
-      else
-        puts "#{player1_name}: #{player1.lives}/3 vs #{player2_name}: #{player2.lives}/3"
-        puts "* * * * * * * !  NEW TURN   ! * *  * * * * * *"
-        puts 
-      end
+    
+    puts "#{@player1_name}: #{@player1.lives}/3 vs #{@player2_name}: #{@player2.lives}/3"
+  end
+
+
+loop do
+  turns(@player1)
+    if @player1.gameover
+      break puts "#{@player2_name} WINS with a score of #{@player2.lives}/3" 
     end
+
+  turns(@player2)
+    if @player2.gameover
+      break puts "#{@player1_name} WINS with a score of #{@player1.lives}/3" 
+    end
+  puts "* * * * * * * !  NEW TURN   ! * *  * * * * * *"
 end
+
+  puts "- - - - - - - - !  GAMEOVER  ! - - - - - - - -"
+  puts "- - - - - - - - !  GOODBYE   ! - - - - - - - -"
