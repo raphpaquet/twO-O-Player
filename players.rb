a5342f879c8e50e9d14cbd509b952a55785e95c7
raphpaquet <raphaelle.paquet@hotmail.com>
2021-01-04 18:33:01 +0000

    classes done

--- players.rb
+++ players.rb
@@ -1,7 +1,16 @@
 class Players
-
+attr_accessor :lives, :name
   def initialize (name, lives)
     @lives = 3
     @name = name
   end
+
+  def incorrect
+    @lives -= 1
+  end
+
+  def gameover
+    @lives == 0
+  end
 end 
+
--- questions.rb
+++ questions.rb
@@ -1,2 +1,9 @@
 class Questions
+  attr_accessor :answer, :show_question 
+  def initialize
+    @number1 = rand(1..20)
+    @number2 = rand(1..20)
+    @answer = @number1 + @number2
+    @show_question = "What does #{@number1} plus #{@number2} equal ?"
+  end
 end 
